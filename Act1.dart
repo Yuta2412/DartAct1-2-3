b0d5437f1de2dd82cd1e02b5299f06323acd9e00
Yuta2412 <santiago.justin2412@gmail.com>
2025-08-07 12:59:09 +0000

    Activity 1, 2, 3 : Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,12 @@
+// void main() {
+//   String name = "Alice";
+//   print("Welcome, $name!");
+// }
+
+// void main() {
+//   dynamic name = "Alice";
+//   name = 25;
+//   print("Welcome, $name!");
+// }
+
+// Dynamic variable can be converted the string to interger value. It can printed the latest variable value.
